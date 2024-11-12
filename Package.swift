a7122ce0b9e1e3071deaddd2dea6946af5744163
Barna Nemeth <nemethbarna9@gmail.com>
2024-11-12 14:58:13 +0100

    feat: add swift collections

--- Package.swift
+++ Package.swift
@@ -8,11 +8,18 @@ let package = Package(
     platforms: [.macOS(.v14)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-collections/", from: "1.1.4")
     ],
     targets: [
         .executableTarget(
             name: "advent-of-code-2024",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")],
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "OrderedCollections", package: "swift-collections"),
+                .product(name: "BitCollections", package: "swift-collections"),
+                .product(name: "HashTreeCollections", package: "swift-collections"),
+                .product(name: "SortedCollections", package: "swift-collections"),
+            ],
             resources: [
                 .process("ExampleInputs/")
             ]
