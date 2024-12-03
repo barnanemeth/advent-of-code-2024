f8ab092bfa97a686f4ee05d2fbd7c6ab09c3805b
Barna Nemeth <nemethbarna9@gmail.com>
2024-12-03 09:12:14 +0100

    feat: solve day03

--- Sources/Days/Day03.swift
+++ Sources/Days/Day03.swift
@@ -7,16 +7,64 @@
 
 import Foundation
 
-final class Day03: DayBase { }
+fileprivate struct Multiplication {
+    let numbers: [Int]
+
+    var result: Int { numbers.reduce(1, *) }
+
+    init?(substring: String) {
+        let components = substring
+            .replacingOccurrences(of: "mul(", with: "")
+            .replacingOccurrences(of: ")", with: "")
+            .components(separatedBy: ",")
+        guard let firstString = components.first,
+              let first = Int(firstString),
+              let lastString = components.last,
+              let last = Int(lastString) else { return nil }
+        numbers = [first, last]
+    }
+}
+
+final class Day03: DayBase {
+    private let multiplications: [Multiplication]
+    private let multiplicationsWithControls: [Multiplication]
+
+    required init(inputContent: String) {
+        multiplications = inputContent
+            .trimmed()
+            .matches(for: "mul\\([0-9]{1,3},[0-9]{1,3}\\)")
+            .compactMap { Multiplication(substring: $0) }
+
+        var isEnabled = true
+        multiplicationsWithControls = inputContent
+            .trimmed()
+            .matches(for: #"don't\(\)|do\(\)|mul\([0-9]{1,3},[0-9]{1,3}\)"#)
+            .reduce(into: [Multiplication]()) { multiplications, item in
+                if isEnabled, let multiplication = Multiplication(substring: item) {
+                    multiplications.append(multiplication)
+                }
+
+                switch item {
+                case "don't()":
+                    isEnabled = false
+                case "do()":
+                    isEnabled = true
+                default: break
+                }
+            }
+
+        super.init(inputContent: inputContent)
+    }
+}
 
 // MARK: - Day
 
 extension Day03: Day {
     func partOne() throws -> CustomStringConvertible {
-        throw PartUnimplemented()
+        multiplications.map(\.result).reduce(0, +)
     }
 
     func partTwo() throws -> CustomStringConvertible {
-        throw PartUnimplemented()
+        multiplicationsWithControls.map(\.result).reduce(0, +)
     }
 }
--- Sources/Helpers/String+Extensions.swift
+++ Sources/Helpers/String+Extensions.swift
@@ -27,4 +27,18 @@ extension String {
     func getLines() -> [String] {
         components(separatedBy: "\n")
     }
+
+    func matches(for regex: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: self,
+                                        range: NSRange(startIndex..., in: self))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
 }
