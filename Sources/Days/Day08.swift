d52b0b7ad8b8b7b549767f7fbc46ebba6e5b86b8
Barna Nemeth <nemethbarna9@gmail.com>
2024-12-08 12:01:50 +0100

    feat: solve day08

--- Sources/Days/Day08.swift
+++ Sources/Days/Day08.swift
@@ -7,16 +7,88 @@
 
 import Foundation
 
-final class Day08: DayBase { }
+fileprivate enum MapItem: Equatable {
+    case empty
+    case antenna(Character)
+    case antinode
+
+    var anetnnaCharacter: Character? {
+        switch self {
+        case let .antenna(character): character
+        default: nil
+        }
+    }
+
+    init(character: Character) {
+        switch character {
+        case ".": self = .empty
+        default:
+            guard character.isLetter || character.isNumber else { fatalError() }
+            self = .antenna(character)
+        }
+    }
+}
+
+final class Day08: DayBase {
+    private let map: [Point: MapItem]
+
+    required init(inputContent: String) {
+        let points = inputContent.trimmed().getLines().enumerated().flatMap { y, line in
+            line.enumerated().map { x, character in
+                (Point(x, y), MapItem(character: character))
+            }
+        }
+        map = Dictionary(points, uniquingKeysWith: { _, item in item })
+
+        super.init(inputContent: inputContent)
+    }
+
+    private func searchingAntinodes(withTFrequencyAntennas: Bool) -> [Point: MapItem] {
+        var map = map
+        let antennas = map.filter { $0.value != .empty }
+        Set(antennas.compactMap(\.value.anetnnaCharacter)).forEach { character in
+            let matching = antennas.filter { $0.value.anetnnaCharacter == character }.map(\.key)
+            matching.combinations(2).forEach { combination in
+                let first = combination.first!
+                let last = combination.last!
+                let diff = first - last
+
+                if withTFrequencyAntennas {
+                    var point = first
+                    while map[point] != nil {
+                        map[point] = .antinode
+                        point = point + diff
+                    }
+                    point = last
+                    while map[point] != nil {
+                        map[point] = .antinode
+                        point = point - diff
+                    }
+                } else {
+                    let newFirst = first + diff
+                    if map[newFirst] != nil {
+                        map[newFirst] = .antinode
+                    }
+
+                    let newLast = last - diff
+                    if map[newLast] != nil {
+                        map[newLast] = .antinode
+                    }
+                }
+            }
+        }
+        return map
+    }
+}
 
 // MARK: - Day
 
 extension Day08: Day {
     func partOne() async throws -> CustomStringConvertible {
-        throw PartUnimplemented()
+        searchingAntinodes(withTFrequencyAntennas: false).filter { $0.value == .antinode }.count
     }
     
     func partTwo() throws -> CustomStringConvertible {
-        throw PartUnimplemented()
+        searchingAntinodes(withTFrequencyAntennas: true).filter { $0.value == .antinode }.count
     }
 }
--- Sources/Helpers/Sequence+Extensions.swift
+++ Sources/Helpers/Sequence+Extensions.swift
@@ -23,4 +23,23 @@ extension Array {
     subscript(safe index: Index) -> Element? {
         indices.contains(index) ? self[index] : nil
     }
+
+    func combinations(_ count: Int) -> [[Element]] {
+        guard count > 0 else { return [[]] }
+        guard count <= count else { return [] }
+
+        if count == 1 {
+            return map { [$0] }
+        }
+
+        var result: [[Element]] = []
+
+        for (index, element) in self.enumerated() {
+            let remainingElements = Array(self[(index + 1)...])
+            let subcombinations = remainingElements.combinations(count - 1)
+            result += subcombinations.map { [element] + $0 }
+        }
+
+        return result
+    }
 }
